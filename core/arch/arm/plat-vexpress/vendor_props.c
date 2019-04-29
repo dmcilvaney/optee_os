b953dafbcea8a19bffd552d2803d41d80b566cbd
Daniel McIlvaney <damcilva@microsoft.com>
2019-04-29 14:44:30 -0700

    Improve vendor_props example in vexpress

--- core/arch/arm/plat-vexpress/vendor_props.c
+++ core/arch/arm/plat-vexpress/vendor_props.c
@@ -10,49 +10,52 @@
 #include <tee/tee_cryp_utl.h>
 
 /*
+ * This property generates a seed value
+ * for each TA (as identified by its UUID)
+ * which is tied to the hardware.
+ * 
+ * Note: Use tee_otp_get_hw_unique_key() instead
+ * of tee_otp_get_die_id() to generate a seed
+ * which is uinique for individual devices rather
+ * than the more generic device type as might be
+ * provided by the die ID.
+ * 
  * The data to hash is 48 bytes made up of:
  * - 16 bytes: the UUID of the calling TA.
- * - 32 bytes: the hardware device ID
- * The resulting endorsement seed is 32 bytes.
+ * - 32 bytes: the hardware die ID
+ * 
+ * The resulting endorsement seed is the 
+ * SHA256 hash of the above data.
  *
- * The output buffer is the "binary" struct defined in
- * the "prop_value" union and therefore comprises:
- * -  4 bytes: the size of the binary value data (32)
- * - 32 bytes: the binary value data (endorsement seed)
- *
- * Note that this code assumes an endorsement seed
- * size == device ID size for convenience.
  */
 static TEE_Result get_prop_endorsement(struct tee_ta_session *sess,
 				       void *buf, size_t *blen)
 {
 	TEE_Result res;
-	uint32_t ta_endorsement_seed_size = 32;
-	uint8_t data[sizeof(TEE_UUID) + ta_endorsement_seed_size];
-	uint32_t bin[1 + ta_endorsement_seed_size / sizeof(uint32_t)];
-	uint32_t *bin_len = (uint32_t *)bin;
-	uint8_t *bin_val = (uint8_t *)(&bin[1]);
+	uint8_t ta_endorsement_seed[TEE_SHA256_HASH_SIZE];
+	uint32_t ta_endorsement_seed_size = sizeof(ta_endorsement_seed);
+	uint8_t input_data[sizeof(TEE_UUID) + TEE_SHA256_HASH_SIZE];
 
-	if (*blen < sizeof(bin)) {
-		*blen = sizeof(bin);
+	if (*blen < ta_endorsement_seed_size) {
+		*blen =ta_endorsement_seed_size;
 		return TEE_ERROR_SHORT_BUFFER;
 	}
-	*blen = sizeof(bin);
+	*blen = ta_endorsement_seed_size;
 
-	memcpy(data, &sess->ctx->uuid, sizeof(TEE_UUID));
+	// Copy UUID into the data to be hashed
+	memcpy(input_data, &sess->ctx->uuid, sizeof(TEE_UUID));
 
-	if (tee_otp_get_die_id(&data[sizeof(TEE_UUID)],
-			       ta_endorsement_seed_size))
+	// Add the die ID after the UUID
+	if (tee_otp_get_die_id(&input_data[sizeof(TEE_UUID)],
+			       TEE_SHA256_HASH_SIZE))
 		return TEE_ERROR_BAD_STATE;
 
-	res = tee_hash_createdigest(TEE_ALG_SHA256, data, sizeof(data),
-				    bin_val, ta_endorsement_seed_size);
+	res = tee_hash_createdigest(TEE_ALG_SHA256, input_data, sizeof(input_data),
+				    ta_endorsement_seed, ta_endorsement_seed_size);
 	if (res != TEE_SUCCESS)
 		return TEE_ERROR_BAD_STATE;
 
-	*bin_len = ta_endorsement_seed_size;
-
-	return tee_svc_copy_to_user((void *)buf, bin, sizeof(bin));
+	return tee_svc_copy_to_user((void *)buf, ta_endorsement_seed, ta_endorsement_seed_size);
 }
 
 static const struct tee_props vendor_propset_array_tee[] = {
